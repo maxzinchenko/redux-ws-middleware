0656fa5abc64f5a625c7fbf444d9d66b386c55e3
Max Zinchenko <zzeenj@gmail.com>
2022-01-11 22:06:25 +0200

    remove build types folder

--- typedef/middleware/Socket/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { MiddlewareOptions, SocketAction, MiddlewareAPI } from '../../typedef';
-export declare const createSocketMiddleware: <Req, Res, SReq = Req, DRes = Res>({ actionTypes, completedActionTypes, ...socketOptions }: MiddlewareOptions<Req, Res, SReq, DRes>) => ({ dispatch }: MiddlewareAPI) => (next: (action: SocketAction) => void) => (action: SocketAction) => void;
--- typedef/services/Socket/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Dispatch } from '../../typedef';
-import { Options } from './typedef';
-export declare class Socket {
-    #private;
-    constructor(options: Options, dispatch: Dispatch, actionTypes: [string, string]);
-    connect: () => void;
-    sendMessage: <D = {}>(data: D) => void;
-    disconnect: () => void;
-}
